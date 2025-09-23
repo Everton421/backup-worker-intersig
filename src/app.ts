851527bde29c7d47df4872f2b09f9c6ac3301715
unknown <evertonjose421@gmail.com>
2025-09-23 17:54:27 -0300

    230920251754

--- src/app.ts
+++ src/app.ts
@@ -8,13 +8,39 @@ import { getClientesPorCodigo } from "./routes/get-client-codigo/get-clientes-co
 import { testeConexaoBanco } from "./routes/teste-conexao-banco-cliente/teste-conexao-banco-cliente.ts";
 import { executarBackup } from "./routes/executar-backup/executar-backup.ts";
 import { getDatabases } from "./routes/get-databases/get-databases.ts";
+import fs from  'node:fs' ;
+import path from 'node:path'
+
+
+    let certPathEnv ; 
+    if(process.env.PATH_CERT){
+          certPathEnv  = String(process.env.PATH_CERT) 
+    }
+
+    let keyPathEnv;
+    if(process.env.PATH_KEY){
+          keyPathEnv  = String(process.env.PATH_KEY) 
+    }
+
+
+
+    let httpsOptions={}
+
+ if( process.env.NODE_ENV === 'production' && keyPathEnv && certPathEnv ){
+        const keyPath = path.join(keyPathEnv );
+        const certPath = path.join(certPathEnv);
+
+        httpsOptions = {
+        key: fs.readFileSync(keyPath),
+        cert: fs.readFileSync(certPath)
+         }
+      }
+
 
 const server = fastify(
-    {
-         logger: false
-           
-             
-           
+        {
+           logger: false,
+            https:httpsOptions
          }
 ).withTypeProvider<ZodTypeProvider>()
 
--- src/server.ts
+++ src/server.ts
@@ -7,4 +7,5 @@ import { server } from "./app.ts";
     }
 
 
+  
     server.listen( { port: port , host: '0.0.0.0' } ,()=>console.log(` Serve is running post ${port} `))
