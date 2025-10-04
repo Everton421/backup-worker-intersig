bd61c3878e23c9d611215e58b4cdef0564c42c6f
unknown <evertonjose421@gmail.com>
2025-10-04 10:43:50 -0300

    041020251043

--- src/services/auto-exe-backup.ts
+++ src/services/auto-exe-backup.ts
@@ -40,6 +40,9 @@ export async function  execBackup (codigoCliente:number, config:mysqlConfig, dat
 
     try{
          if( databases.length > 0 ){
+               await db.update(clientes)
+                            .set(  {  status_backup: 'em-andamento',  msg_backup:`backup do dia ${data} ${hora} em andamento`  }).
+                            where(eq(clientes.codigo, codigoCliente))
              for( const  database  of databases ){
                 
                 await dumpDatabase(config, database, id ).then(result => {
