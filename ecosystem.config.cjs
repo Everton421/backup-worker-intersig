a5e63560b99195307a524cf9056577dec12aea32
unknown <evertonjose421@gmail.com>
2025-10-18 15:18:38 -0300

    181020251514

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -36,7 +36,7 @@ module.exports = {
       exec_mode: "fork", // Use 'fork' para processos independentes
       watch: false,
       max_memory_restart: "500M", // Aumente a memória se os backups forem intensos
-      instances: 4, // Defina o número de workers que você deseja rodar
+      instances: 2, // Defina o número de workers que você deseja rodar
       autorestart: true,
       restart_delay: 5000,
       error_file: "logs/backup-worker-err.log",
