4bb87c19062fd5f897ecef6c4cf4a83659024b91
unknown <evertonjose421@gmail.com>
2026-02-20 14:34:45 -0300

    first commit

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,30 +1,7 @@
 // ecosystem.config.cjs
 module.exports = {
   apps: [
-      {
-      name: "api-backup",
-      // O script principal agora é o executável CLI do tsx
-      script: "node_modules/tsx/dist/cli.cjs", // Use o caminho relativo correto ou absoluto
-      
-        interpreter: "node",
-        // Os argumentos para o "script" (cli.js do tsx)
-        args: [
-          "--env-file", // Argumento para o tsx
-          ".env",       // Valor do argumento --env-file
-          "--experimental-strip-types", // Outro argumento para o tsx
-          "src/server.ts" // O arquivo que o tsx deve processar, passado como argumento final para o tsx
-        ],
-       
-        exec_mode: "fork",
-        watch: false,
-        max_memory_restart: "1G",
-        instances: 1,
-        autorestart: true,
-        restart_delay: 5000,
-        error_file: "logs/api-backup-err.log",
-        out_file: "logs/api-backup-out.log",
-        log_date_format: "YYYY-MM-DD HH:mm:ss"
-      } , 
+   
     {
       name: "backup-worker", // Nome do seu worker
       script: "node_modules/tsx/dist/cli.cjs", // Usa o tsx para executar o worker
@@ -33,7 +10,7 @@ module.exports = {
         "--env-file",
         ".env",
         "--experimental-strip-types",
-        "src/workers/worker.ts" // O arquivo do seu worker
+        "src/index.ts" // O arquivo do seu worker
       ],
       exec_mode: "fork", // Use 'fork' para processos independentes
       watch: false,
